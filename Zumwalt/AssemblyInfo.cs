2e0ed90461c039acc31585175c7c2805609dfb29
Riketta <rowneg@bk.ru>
2014-06-20 13:08:00 +1000

    AssemblyInfo update

--- Zumwalt/AssemblyInfo.cs
+++ Zumwalt/AssemblyInfo.cs
@@ -3,7 +3,7 @@
 [assembly: System.Reflection.AssemblyTrademark("")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("356720a4-fa40-4b47-91d2-b2c21a6b682e")]
-[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9 EquiFox17 & xEnt 2014")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9 Riketta")]
 [assembly: System.Runtime.CompilerServices.RuntimeCompatibility(WrapNonExceptionThrows=true)]
 [assembly: System.Reflection.AssemblyFileVersion("1.0.*")]
 [assembly: System.Diagnostics.Debuggable(System.Diagnostics.DebuggableAttribute.DebuggingModes.IgnoreSymbolStoreSequencePoints)]
@@ -11,6 +11,6 @@
 [assembly: System.Reflection.AssemblyDescription("")]
 [assembly: System.Reflection.AssemblyTitle("Zumwalt")]
 [assembly: System.Reflection.AssemblyConfiguration("")]
-[assembly: System.Reflection.AssemblyCompany("EquiFox17 & xEnt")]
+[assembly: System.Reflection.AssemblyCompany("")]
 [assembly: System.Reflection.AssemblyProduct("Zumwalt")]
 
--- Zumwalt_Patcher/AssemblyInfo.cs
+++ Zumwalt_Patcher/AssemblyInfo.cs
@@ -1,11 +1,11 @@
 ﻿// Assembly Zumwalt_Patcher, Version 1.0.0.0
 
-[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9 EquiFox17 2014")]
+[assembly: System.Reflection.AssemblyCopyright("Copyright \x00a9 Riketta")]
 [assembly: System.Reflection.AssemblyDescription("")]
 [assembly: System.Reflection.AssemblyConfiguration("")]
-[assembly: System.Reflection.AssemblyCompany("EquiFox17")]
-[assembly: System.Reflection.AssemblyProduct("Rust++ Patcher")]
-[assembly: System.Reflection.AssemblyTitle("Rust++ Patcher")]
+[assembly: System.Reflection.AssemblyCompany("")]
+[assembly: System.Reflection.AssemblyProduct("Zumwalt Patcher")]
+[assembly: System.Reflection.AssemblyTitle("Zumwalt Patcher")]
 [assembly: System.Reflection.AssemblyTrademark("")]
 [assembly: System.Runtime.InteropServices.ComVisible(false)]
 [assembly: System.Runtime.InteropServices.Guid("861b169f-9f84-4ff9-a58d-b3f53a1e5ee1")]

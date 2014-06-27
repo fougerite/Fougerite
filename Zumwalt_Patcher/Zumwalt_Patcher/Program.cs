cbe6b4f1e4a6c7e1c1fabf80c102a79f35a54cd4
Riketta <rowneg@bk.ru>
2014-06-27 21:14:04 +1000

    HowTo added

--- Zumwalt_Patcher/Zumwalt_Patcher/Program.cs
+++ Zumwalt_Patcher/Zumwalt_Patcher/Program.cs
@@ -10,31 +10,38 @@ namespace Zumwalt_Patcher
 
         private static void Main(string[] args)
         {
-            bool firstPass = args.Contains ("-1");
-            bool secondPass = args.Contains ("-2");
+            bool firstPass = args.Contains("-1");
+            bool secondPass = args.Contains("-2");
 
             Logger.Clear();
 
             if (!firstPass && !secondPass) 
             {
-                Logger.Log ("No command specified.");
+                Logger.Log("No command specified.");
+                Logger.Log("Launch patcher with args: \"-1\" (fields update) or\\and \"-2\" (methods update).");
+                Logger.Log("Or enter \"0\" to patch with both flags");
+                if (Console.ReadLine() == "0")
+                {
+                    firstPass = true;
+                    secondPass = true;
+                }
             }
 
-            ILPatcher patcher = new ILPatcher ();
+            ILPatcher patcher = new ILPatcher();
 
             bool result = true;
             if (firstPass) 
             {
-                result = result && patcher.FirstPass ();
+                result = result && patcher.FirstPass();
             }
 
             if (secondPass) 
             {
-                result = result && patcher.SecondPass ();
+                result = result && patcher.SecondPass();
             }
 
             if (result) {
-                Logger.Log ("The patch was applied successfully!");
+                Logger.Log("The patch was applied successfully!");
             }
         }
     }

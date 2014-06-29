0a3f41d3dbb57912c6086975526987f6a3bdc2b7
Riketta <rowneg@bk.ru>
2014-06-29 12:00:25 +0700

    Logger optimization

--- Zumwalt/Zumwalt/Logger.cs
+++ Zumwalt/Zumwalt/Logger.cs
@@ -9,6 +9,7 @@ namespace Zumwalt
     public static class Logger
     {
         private static string LogsFolder = @".\logs\";
+        private static StreamWriter LogWriter;
 
         public static void Init()
         {
@@ -21,40 +22,47 @@ namespace Zumwalt
             return Text;
         }
 
+        private static void WriteLog(string Message)
+        {
+            using (LogWriter = new StreamWriter(LogsFolder + "Log " + DateTime.Now.ToString("dd_MM_yyyy") + ".txt", true))
+                LogWriter.WriteLine(LogFormat(Message));
+        }
+
         public static void Log(string Message, UnityEngine.Object Context = null)
         {
-            Logger.Log(Message, Context);
-            using (StreamWriter Writer = new StreamWriter(LogsFolder + "Log.txt", true))
-                Writer.WriteLine(LogFormat(Message));
+            Debug.Log(Message, Context);
+            Message = "[Console]" + Message;
+            WriteLog(Message);
         }
 
         public static void LogWarning(string Message, UnityEngine.Object Context = null)
         {
             Debug.LogWarning(Message, Context);
-            using (StreamWriter Writer = new StreamWriter(LogsFolder + "LogWarning.txt", true))
-                Writer.WriteLine(LogFormat(Message));
+            Message = "[Warning]" + Message;
+            WriteLog(Message);
         }
 
         public static void LogError(string Message, UnityEngine.Object Context = null)
         {
             Debug.LogError(Message, Context);
-            using (StreamWriter Writer = new StreamWriter(LogsFolder + "LogError.txt", true))
-                Writer.WriteLine(LogFormat(Message));
+            Message = "[Error]" + Message;
+            WriteLog(Message);
         }
 
         public static void LogException(Exception Ex, UnityEngine.Object Context = null)
         {
             Debug.LogException(Ex, Context);
-            using (StreamWriter Writer = new StreamWriter(LogsFolder + "LogException.txt", true))
-                Writer.WriteLine(LogFormat(Ex.ToString()));
+            string Message = "[Exception]" + Ex.ToString();
+            WriteLog(Message);
         }
 
         public static void ChatLog(string Sender, string Msg)
         {
             Msg = "[CHAT] " + Sender + ": " + Msg;
-            Logger.Log(Msg);
-            using (StreamWriter Writer = new StreamWriter(LogsFolder + DateTime.Now.Month + "-" + DateTime.Now.Day + " ChatLog.txt", true))
-                Writer.WriteLine(LogFormat(Sender + ": " + Msg));
+            Debug.Log(Msg);
+
+            using (LogWriter = new StreamWriter(LogsFolder + "Chat " + DateTime.Now.ToString("dd_MM_yyyy") + ".txt", true))
+                LogWriter.WriteLine(LogFormat(Msg));
         }
     }
 }

5394ef6d9b2c85426a3ac8ae42302838bef81c05
Riketta <rowneg@bk.ru>
2014-07-01 18:19:38 +0300

    ModuleLoader Logging

--- Fougerite/Fougerite/ModuleManager.cs
+++ Fougerite/Fougerite/ModuleManager.cs
@@ -17,8 +17,7 @@ namespace Fougerite
         {
             this.ApiVersion = version;
         }
-        public ApiVersionAttribute(int major, int minor)
-            : this(new Version(major, minor))
+        public ApiVersionAttribute(int major, int minor) : this(new Version(major, minor))
         {
         }
     }
@@ -27,18 +26,18 @@ namespace Fougerite
     {
         public static readonly Version ApiVersion = new Version(1, 0, 0, 0);
         private static string ModulesFolder = @".\Modules\";
-        private static bool IgnoreVersion = false;
-        private static readonly Dictionary<string, Assembly> loadedAssemblies = new Dictionary<string, Assembly>();
-        private static readonly List<ModuleContainer> plugins = new List<ModuleContainer>();
+        private static bool IsIgnoreVersion = true;
+        private static readonly Dictionary<string, Assembly> LoadedAssemblies = new Dictionary<string, Assembly>();
+        private static readonly List<ModuleContainer> Modules = new List<ModuleContainer>();
 
         public static ReadOnlyCollection<ModuleContainer> Plugins
         {
-            get { return new ReadOnlyCollection<ModuleContainer>(plugins); }
+            get { return new ReadOnlyCollection<ModuleContainer>(Modules); }
         }
 
         internal static void LoadModules()
         {
-
+            Logger.Log("Loading modules...");
             string IgnoredPluginsFilePath = Path.Combine(ModulesFolder, "ignoredplugins.txt");
 
             List<string> IgnoredFiles = new List<string>();
@@ -47,138 +46,148 @@ namespace Fougerite
 
             FileInfo[] FileInfos = new DirectoryInfo(ModulesFolder).GetFiles("*.dll");
 
-            Dictionary<Module, Stopwatch> pluginInitWatches = new Dictionary<Module, Stopwatch>();
-            foreach (FileInfo fileInfo in FileInfos)
+            Dictionary<Module, Stopwatch> PluginInitWatches = new Dictionary<Module, Stopwatch>();
+            foreach (FileInfo FileInfo in FileInfos)
             {
-                string fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fileInfo.Name);
-                if (IgnoredFiles.Contains(fileNameWithoutExtension))
+                Logger.Log("Module Found: " + FileInfo.Name);
+                string FileNameWithoutExtension = Path.GetFileNameWithoutExtension(FileInfo.Name);
+                if (IgnoredFiles.Contains(FileNameWithoutExtension))
                 {
-                    Logger.LogWarning(string.Format("{0} was ignored from being loaded.", fileNameWithoutExtension));
+                    Logger.LogWarning(string.Format("{0} was ignored from being loaded.", FileNameWithoutExtension));
                     continue;
                 }
 
                 try
                 {
-                    Assembly assembly;
+                    Logger.Log("Loading " + FileInfo.Name);
+                    Assembly Assembly;
                     // The plugin assembly might have been resolved by another plugin assembly already, so no use to
                     // load it again, but we do still have to verify it and create plugin instances.
-                    if (!loadedAssemblies.TryGetValue(fileNameWithoutExtension, out assembly))
+                    if (!LoadedAssemblies.TryGetValue(FileNameWithoutExtension, out Assembly))
                     {
-                        assembly = Assembly.Load(File.ReadAllBytes(fileInfo.FullName));
-                        loadedAssemblies.Add(fileNameWithoutExtension, assembly);
+                        Assembly = Assembly.Load(File.ReadAllBytes(FileInfo.FullName));
+                        LoadedAssemblies.Add(FileNameWithoutExtension, Assembly);
                     }
 
-                    foreach (Type type in assembly.GetExportedTypes())
+                    foreach (Type Type in Assembly.GetExportedTypes())
                     {
-                        if (!type.IsSubclassOf(typeof(Module)) || !type.IsPublic || type.IsAbstract)
+                        Logger.Log("Checking " + Type.FullName);
+                        // Не сын, приватный или абстрактный
+                        if (!Type.IsSubclassOf(typeof(Module)) || !Type.IsPublic || Type.IsAbstract)
                             continue;
-                        object[] customAttributes = type.GetCustomAttributes(typeof(ApiVersionAttribute), false);
-                        if (customAttributes.Length == 0)
-                            continue;
+                        Logger.Log("Checked " + Type.FullName);
 
-                        if (!IgnoreVersion)
+                        object[] CustomAttributes = Type.GetCustomAttributes(typeof(ApiVersionAttribute), false);
+                        if (CustomAttributes.Length == 0)
+                            continue;
+                        Logger.Log("Ok " + Type.FullName);
+
+                        if (!IsIgnoreVersion)
                         {
-                            var apiVersionAttribute = (ApiVersionAttribute)customAttributes[0];
-                            Version apiVersion = apiVersionAttribute.ApiVersion;
-                            if (apiVersion.Major != ApiVersion.Major || apiVersion.Minor != ApiVersion.Minor)
+                            var ApiVersionAttribute = (ApiVersionAttribute)CustomAttributes[0];
+                            Version ApiVersion = ApiVersionAttribute.ApiVersion;
+                            if (ApiVersion.Major != ApiVersion.Major || ApiVersion.Minor != ApiVersion.Minor)
                             {
                                 Logger.LogWarning(string.Format("Plugin \"{0}\" is designed for a different API version ({1}) and was ignored.",
-                                    type.FullName, apiVersion.ToString(2)));
+                                    Type.FullName, ApiVersion.ToString(2)));
                                 continue;
                             }
                         }
 
-                        Module PluginInstance;
+                        Module PluginInstance = null;
                         try
                         {
-                            Stopwatch initTimeWatch = new Stopwatch();
-                            initTimeWatch.Start();
-                            PluginInstance = (Module)Activator.CreateInstance(type);
-                            initTimeWatch.Stop();
-                            pluginInitWatches.Add(PluginInstance, initTimeWatch);
+                            Stopwatch InitTimeWatch = new Stopwatch();
+                            InitTimeWatch.Start();
+                            PluginInstance = (Module)Activator.CreateInstance(Type);
+                            InitTimeWatch.Stop();
+                            PluginInitWatches.Add(PluginInstance, InitTimeWatch);
                         }
                         catch (Exception ex)
                         {
                             // Broken plugins better stop the entire server init.
-                            throw new InvalidOperationException(
-                                string.Format("Could not create an instance of plugin class \"{0}\".", type.FullName), ex);
+                            Logger.LogError(string.Format("Could not create an instance of plugin class \"{0}\". {1}", Type.FullName, ex));
                         }
-                        plugins.Add(new ModuleContainer(PluginInstance));
+                        if (PluginInstance != null)
+                        {
+                            Modules.Add(new ModuleContainer(PluginInstance));
+                            Logger.Log("Loaded " + FileInfo.Name);
+                            break;
+                        }
                     }
                 }
                 catch (Exception ex)
                 {
                     // Broken assemblies / plugins better stop the entire server init.
-                    throw new InvalidOperationException(
-                        string.Format("Failed to load assembly \"{0}\".", fileInfo.Name), ex);
+                    Logger.LogError(string.Format("Failed to load assembly \"{0}\". {1}", FileInfo.Name, ex));
                 }
             }
 
-            IOrderedEnumerable<ModuleContainer> orderedPluginSelector =
+            IOrderedEnumerable<ModuleContainer> OrderedModuleSelector =
                 from x in Plugins
                 orderby x.Plugin.Order, x.Plugin.Name
                 select x;
 
-            foreach (ModuleContainer current in orderedPluginSelector)
+            foreach (ModuleContainer CurrentModule in OrderedModuleSelector)
             {
-                Stopwatch initTimeWatch = pluginInitWatches[current.Plugin];
-                initTimeWatch.Start();
+                Stopwatch InitTimeWatch = PluginInitWatches[CurrentModule.Plugin];
+                InitTimeWatch.Start();
 
                 try
                 {
-                    current.Initialize();
+                    CurrentModule.Initialize();
                 }
                 catch (Exception ex)
                 {
                     // Broken plugins better stop the entire server init.
-                    throw new InvalidOperationException(string.Format(
-                        "Plugin \"{0}\" has thrown an exception during initialization.", current.Plugin.Name), ex);
+                    Logger.LogError(string.Format(
+                        "Plugin \"{0}\" has thrown an exception during initialization. {1}", CurrentModule.Plugin.Name, ex));
                 }
 
-                initTimeWatch.Stop();
+                InitTimeWatch.Stop();
                 Logger.Log(string.Format(
-                    "Plugin {0} v{1} (by {2}) initiated.", current.Plugin.Name, current.Plugin.Version, current.Plugin.Author));
+                    "Plugin {0} v{1} (by {2}) initiated.", CurrentModule.Plugin.Name, CurrentModule.Plugin.Version, CurrentModule.Plugin.Author));
             }
         }
 
-        internal static void UnloadPlugins()
+        internal static void UnloadModules()
         {
-            var pluginUnloadWatches = new Dictionary<ModuleContainer, Stopwatch>();
-            foreach (ModuleContainer pluginContainer in plugins)
+            var ModuleUnloadWatches = new Dictionary<ModuleContainer, Stopwatch>();
+            foreach (ModuleContainer ModuleContainer in Modules)
             {
-                Stopwatch unloadWatch = new Stopwatch();
-                unloadWatch.Start();
+                Stopwatch UnloadWatch = new Stopwatch();
+                UnloadWatch.Start();
 
                 try
                 {
-                    pluginContainer.DeInitialize();
+                    ModuleContainer.DeInitialize();
                 }
                 catch (Exception ex)
                 {
                     Logger.LogError(string.Format(
-                        "Plugin \"{0}\" has thrown an exception while being deinitialized:\n{1}", pluginContainer.Plugin.Name, ex));
+                        "Plugin \"{0}\" has thrown an exception while being deinitialized:\n{1}", ModuleContainer.Plugin.Name, ex));
                 }
 
-                unloadWatch.Stop();
-                pluginUnloadWatches.Add(pluginContainer, unloadWatch);
+                UnloadWatch.Stop();
+                ModuleUnloadWatches.Add(ModuleContainer, UnloadWatch);
             }
 
-            foreach (ModuleContainer pluginContainer in plugins)
+            foreach (ModuleContainer ModuleContainer in Modules)
             {
-                Stopwatch unloadWatch = pluginUnloadWatches[pluginContainer];
-                unloadWatch.Start();
+                Stopwatch UnloadWatch = ModuleUnloadWatches[ModuleContainer];
+                UnloadWatch.Start();
 
                 try
                 {
-                    pluginContainer.Dispose();
+                    ModuleContainer.Dispose();
                 }
                 catch (Exception ex)
                 {
                     Logger.LogError(string.Format(
-                        "Plugin \"{0}\" has thrown an exception while being disposed:\n{1}", pluginContainer.Plugin.Name, ex));
+                        "Plugin \"{0}\" has thrown an exception while being disposed:\n{1}", ModuleContainer.Plugin.Name, ex));
                 }
 
-                unloadWatch.Stop();
+                UnloadWatch.Stop();
             }
         }
     }

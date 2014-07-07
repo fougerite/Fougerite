cc1097f32840979b7930f44e23e48dfd024d0b77
Riketta <rowneg@bk.ru>
2014-07-07 12:51:05 +0300

    Player commands logging added

--- Fougerite/Fougerite/Hooks.cs
+++ Fougerite/Fougerite/Hooks.cs
@@ -102,6 +102,7 @@
                 string item = Facepunch.Utility.String.QuoteSafe(arg.argUser.user.Displayname);
                 string str = Facepunch.Utility.String.QuoteSafe(arg.GetString(0, "text"));
 
+                Logger.LogDebug("Player " + item + " execute " + arg.ArgsStr); // arg.GetString(0)
 
                 if (OnChatReceived != null)
                     OnChatReceived(ref arg);

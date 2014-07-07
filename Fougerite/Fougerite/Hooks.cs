b7e1f2c9b85b03b2995185d07c11349bc4d4a675
Riketta <rowneg@bk.ru>
2014-07-07 13:04:28 +0300

    ChatCommang logger fix

--- Fougerite/Fougerite/Hooks.cs
+++ Fougerite/Fougerite/Hooks.cs
@@ -102,7 +102,8 @@
                 string item = Facepunch.Utility.String.QuoteSafe(arg.argUser.user.Displayname);
                 string str = Facepunch.Utility.String.QuoteSafe(arg.GetString(0, "text"));
 
-                Logger.LogDebug("[CHAT-CMD] " + item + " executed " + arg.ArgsStr); // arg.GetString(0)
+                if (arg.GetString(0).StartsWith("/"))
+                    Logger.LogDebug("[CHAT-CMD] " + item + " executed " + arg.ArgsStr); // arg.GetString(0)
 
                 if (OnChatReceived != null)
                     OnChatReceived(ref arg);

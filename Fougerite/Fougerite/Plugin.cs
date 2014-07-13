19eafce36e4939e0f4502d00c41751a2a226b136
Alexander Konovalov <alex.knvl@gmail.com>
2014-07-14 02:31:55 +0400

    Restored null checks in Plugin class.

--- Fougerite/Fougerite/Plugin.cs
+++ Fougerite/Fougerite/Plugin.cs
@@ -417,6 +417,8 @@ namespace Fougerite
 
         public void OnEntityDeployed(Fougerite.Player player, Entity entity)
         {
+            if (player == null)
+                throw new ArgumentNullException("player");
             if (entity == null)
                 throw new ArgumentNullException("entity");
             Invoke("On_EntityDeployed", player, entity);
@@ -452,11 +454,15 @@ namespace Fougerite
 
         public void OnPlayerConnected(Fougerite.Player player)
         {
+            if (player == null)
+                throw new ArgumentNullException("player");
             Invoke("On_PlayerConnected", player);
         }
 
         public void OnPlayerDisconnected(Fougerite.Player player)
         {
+            if (player == null)
+                throw new ArgumentNullException("player");
             Invoke("On_PlayerDisconnected", player);
         }
 

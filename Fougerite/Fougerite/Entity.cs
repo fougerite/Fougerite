64c7799dcc4153cfb521c94bc7b584e776807563
Michael Crews <mike.crews@gmail.com>
2014-07-13 22:49:58 -0700

    Successful build with Xamarin Studio, Mono .Net 3.5 runtime, Mono compiler

--- Fougerite/Fougerite/Entity.cs
+++ Fougerite/Fougerite/Entity.cs
@@ -55,7 +55,7 @@
             System.Collections.Generic.List<Entity> list = new System.Collections.Generic.List<Entity>();
             foreach (StructureComponent component in (this.Object as StructureComponent)._master._structureComponents)
             {
-                if (component != this.Object)
+				if (component != this.Object as StructureComponent)
                 {
                     list.Add(new Entity(component));
                 }

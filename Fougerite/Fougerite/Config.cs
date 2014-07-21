8da1e44d6c0aba75f3eff9bd53c8e85f88151973
Alexander Konovalov <alex.knvl@gmail.com>
2014-07-21 12:29:36 +0400

    Contracts for Fougerite.Config. Fixed possible null ref exception in GetBoolValue.

--- Fougerite/Fougerite/Config.cs
+++ Fougerite/Fougerite/Config.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.IO;
 using System.Text;
 using UnityEngine;
@@ -12,6 +13,9 @@ namespace Fougerite
 
         public static void Init(string ConfigPath)
         {
+            Contract.Requires(!string.IsNullOrEmpty(ConfigPath));
+            Contract.Ensures(FougeriteConfig != null);
+
             if (File.Exists(ConfigPath))
             {
                 FougeriteConfig = new IniParser(ConfigPath);
@@ -22,12 +26,20 @@ namespace Fougerite
 
         public static string GetValue(string Section, string Setting)
         {
+            Contract.Requires(!string.IsNullOrEmpty(Section));
+            Contract.Requires(!string.IsNullOrEmpty(Setting));
+
             return FougeriteConfig.GetSetting(Section, Setting);
         }
 
         public static bool GetBoolValue(string Section,string Setting)
         {
-            return Config.FougeriteConfig.GetSetting(Section, Setting).ToLower() == "true";
+            Contract.Requires(!string.IsNullOrEmpty(Section));
+            Contract.Requires(!string.IsNullOrEmpty(Setting));
+
+            var val = FougeriteConfig.GetSetting(Section, Setting);
+
+            return val != null && val.ToLower() == "true";
         }
     }
 }

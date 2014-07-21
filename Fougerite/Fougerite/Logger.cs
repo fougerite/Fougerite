127a09b60c50700e587fa82bdc9bf335bcf5a31d
Riketta <rowneg@bk.ru>
2014-07-21 23:24:52 +0300

    Logger exception stack classname added

--- Fougerite/Fougerite/Logger.cs
+++ Fougerite/Fougerite/Logger.cs
@@ -145,7 +145,7 @@ namespace Fougerite
             string Trace = "";
             System.Diagnostics.StackTrace stackTrace = new System.Diagnostics.StackTrace();
             for (int i = 1; i < stackTrace.FrameCount; i++)
-                Trace += stackTrace.GetFrame(i).GetMethod().Name + " | ";
+                Trace += stackTrace.GetFrame(i).GetMethod().DeclaringType.Name + "->" + stackTrace.GetFrame(i).GetMethod().Name + " | ";
 
             string Message = "[Exception] [ " + Trace + "]\r\n" + Ex.ToString();
             WriteLog(Message);

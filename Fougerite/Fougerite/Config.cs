6f53f1a420fb0357b6b4579bed9fce0c753ef9e9
Michael Crews <mike.crews@gmail.com>
2014-08-10 14:00:17 -0700

    move DirectoryConfig logic to Config

--- Fougerite/Fougerite/Config.cs
+++ Fougerite/Fougerite/Config.cs
@@ -3,16 +3,30 @@ using System.Collections.Generic;
 using System.Diagnostics.Contracts;
 using System.IO;
 using System.Text;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 namespace Fougerite
 {
     class Config
     {
+        public static IniParser FougeriteDirectoryConfig;
         public static IniParser FougeriteConfig;
 
-        public static void Init(string ConfigPath)
+        public static void Init(string DirectoryConfigPath)
         {
+            Contract.Requires(!string.IsNullOrEmpty(DirectoryConfigPath));
+            Contract.Ensures(FougeriteDirectoryConfig != null);
+
+            if (File.Exists(DirectoryConfigPath))
+            {
+                FougeriteDirectoryConfig = new IniParser(DirectoryConfigPath);
+                Debug.Log("DirectoryConfig " + DirectoryConfigPath + " loaded!");
+            }
+            else Debug.Log("DirectoryConfig " + DirectoryConfigPath + " NOT loaded!");
+
+            string ConfigPath = Path.Combine(GetPublicFolder(), "Fougerite.cfg");
+
             Contract.Requires(!string.IsNullOrEmpty(ConfigPath));
             Contract.Ensures(FougeriteConfig != null);
 
@@ -40,6 +54,20 @@ namespace Fougerite
             var val = FougeriteConfig.GetSetting(Section, Setting);
 
             return val != null && val.ToLower() == "true";
+        }
+
+        public static string GetModulesFolder()
+        {
+            Regex root = new Regex(@"^%RootFolder%", RegexOptions.IgnoreCase);             
+            string path = root.Replace(FougeriteDirectoryConfig.GetSetting("Settings", "ModulesFolder"), Util.GetRootFolder()) + @"\";
+            return Util.NormalizePath(path);
+        }
+
+        public static string GetPublicFolder()
+        {
+            Regex root = new Regex(@"^%RootFolder%", RegexOptions.IgnoreCase);             
+            string path = root.Replace(FougeriteDirectoryConfig.GetSetting("Settings", "PublicFolder"), Util.GetRootFolder()) + @"\";
+            return Util.NormalizePath(path);
         }
     }
 }

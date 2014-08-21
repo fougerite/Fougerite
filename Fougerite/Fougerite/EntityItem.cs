20387319ceb5d0a6dca66849132440356012e1ca
dretax <dretax@gmail.hu>
2014-08-22 01:57:11 +0200

    Update EntityItem.cs
    Thanks balu for code assistance and testing.

--- Fougerite/Fougerite/EntityItem.cs
+++ Fougerite/Fougerite/EntityItem.cs
@@ -1,126 +1,97 @@
-﻿namespace Fougerite
+namespace Fougerite
 {
     using System;
 
     public class EntityItem
     {
-        private Inventory internalInv;
-        private int internalSlot;
+        private readonly Inventory internalInv;
+		private readonly int internalSlot;
 
-        public EntityItem()
-        {
-        }
+        public EntityItem(Inventory inv, int slot)
+		{
+			this.internalInv = inv;
+			this.internalSlot = slot;
+		}
 
-        public EntityItem(ref Inventory inv, int slot)
-        {
-            this.internalInv = inv;
-            this.internalSlot = slot;
-        }
+		public void Drop()
+		{
+			DropHelper.DropItem(this.internalInv, this.Slot);
+		}
 
-        private IInventoryItem GetItemRef()
-        {
-            IInventoryItem item;
-            this.internalInv.GetItem(this.internalSlot, out item);
-            return item;
-        }
+		private IInventoryItem GetItemRef()
+		{
+			IInventoryItem item;
+			this.internalInv.GetItem(this.internalSlot, out item);
+			return item;
+		}
 
-        public void Consume(int qty)
-        {
-            if (!this.IsEmpty())
-            {
-                this.RInventoryItem.Consume(ref qty);
-            }
-        }
+		public bool IsEmpty()
+		{
+			return (this.RInventoryItem == null);
+		}
 
-        public bool IsEmpty()
-        {
-            return (this.RInventoryItem == null);
-        }
+		public IInventoryItem RInventoryItem
+		{
+			get
+			{
+				return this.GetItemRef();
+			}
+		}
 
-        public bool TryCombine(EntityItem pi)
-        {
-            if (this.IsEmpty() || pi.IsEmpty())
-            {
-                return false;
-            }
-            return (this.RInventoryItem.TryCombine(pi.RInventoryItem) != InventoryItem.MergeResult.Failed);
-        }
+		public string Name
+		{
+			get
+			{
+				if (!this.IsEmpty())
+				{
+					return this.RInventoryItem.datablock.name;
+				}
+				return "Empty slot";
+			}
+			set
+			{
+				this.RInventoryItem.datablock.name = value;
+			}
+		}
 
-        public bool TryStack(EntityItem pi)
-        {
-            if (this.IsEmpty() || pi.IsEmpty())
-            {
-                return false;
-            }
-            return (this.RInventoryItem.TryStack(pi.RInventoryItem) != InventoryItem.MergeResult.Failed);
-        }
+		public int Quantity
+		{
+			get
+			{
+				return this.UsesLeft;
+			}
+			set
+			{
+				this.UsesLeft = value;
+			}
+		}
 
-        public IInventoryItem RInventoryItem
-        {
-            get
-            {
-                return this.GetItemRef();
-            }
-            set
-            {
-                this.RInventoryItem = value;
-            }
-        }
+		public int Slot
+		{
+			get
+			{
+				if (!this.IsEmpty())
+				{
+					return this.RInventoryItem.slot;
+				}
+				return this.internalSlot;
+			}
+		}
 
-        public string Name
-        {
-            get
-            {
-                if (!this.IsEmpty())
-                {
-                    return this.RInventoryItem.datablock.name;
-                }
-                return null;
-            }
-            set
-            {
-                this.RInventoryItem.datablock.name = value;
-            }
-        }
-
-        public int Quantity
-        {
-            get
-            {
-                return this.UsesLeft;
-            }
-            set
-            {
-                this.UsesLeft = value;
-            }
-        }
-
-        public int Slot
-        {
-            get
-            {
-                if (!this.IsEmpty())
-                {
-                    return this.RInventoryItem.slot;
-                }
-                return -1;
-            }
-        }
-
-        public int UsesLeft
-        {
-            get
-            {
-                if (!this.IsEmpty())
-                {
-                    return this.RInventoryItem.uses;
-                }
-                return -1;
-            }
-            set
-            {
-                this.RInventoryItem.SetUses(value);
-            }
-        }
+		public int UsesLeft
+		{
+			get
+			{
+				if (!this.IsEmpty())
+				{
+					return this.RInventoryItem.uses;
+				}
+				return -1;
+			}
+			set
+			{
+				this.RInventoryItem.SetUses(value);
+			}
+		}
     }
 }

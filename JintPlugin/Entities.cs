038c4a4883eb20c63010de1e70f72307bd60b411
Michael Crews <mike.crews@gmail.com>
2014-08-27 09:46:19 -0700

    new members, namespace, correct overload

--- JintPlugin/Entities.cs
+++ JintPlugin/Entities.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-
-namespace JintPlugin
+﻿namespace JintPlugin
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
     using UnityEngine;
 
     public class Entities
@@ -13,7 +12,7 @@ namespace JintPlugin
             get
             {
                 return (Lookup<string, StructureMaster>)StructureMaster.AllStructures
-                    .ToLookup(p => p.ownerID.ToString());
+                    .ToLookup(sm => sm.ownerID.ToString(), sm => sm);
             }
         }
 
@@ -22,7 +21,25 @@ namespace JintPlugin
             get 
             {              
                 return (Lookup<string, BasicDoor>)(UnityEngine.Object.FindObjectsOfType(typeof(BasicDoor)) as BasicDoor[])
-                    .ToLookup(p => (p.GetComponent("DeployableObject") as DeployableObject).ownerID.ToString());
+                    .ToLookup(bd => (bd.GetComponent<DeployableObject>() as DeployableObject).ownerID.ToString(), bd => bd);
+            }
+        }
+
+        public static Lookup<string, DeployedRespawn> DeployedRespawns
+        {
+            get
+            {
+                return (Lookup<string, DeployedRespawn>)(UnityEngine.Object.FindObjectsOfType(typeof(DeployedRespawn)) as DeployedRespawn[])
+                    .ToLookup(dr => (dr.GetComponent<DeployableObject>() as DeployableObject).ownerID.ToString(), dr => dr);
+            }
+        }
+
+        public static Lookup<string, SleepingAvatar> SleepingAvatars
+        {
+            get
+            {
+                return (Lookup<string, SleepingAvatar>)(UnityEngine.Object.FindObjectsOfType(typeof(SleepingAvatar)) as SleepingAvatar[])
+                    .ToLookup(sa => (sa.GetComponent<DeployableObject>() as DeployableObject).ownerID.ToString(), sa => sa);
             }
         }
     }

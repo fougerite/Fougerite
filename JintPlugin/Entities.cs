ab461dc1036ab05e5dc48d484ea67e0bc50fb221
Michael Crews <mike.crews@gmail.com>
2014-08-26 22:14:10 -0700

    Entities class

---
+++ JintPlugin/Entities.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JintPlugin
+{
+    using UnityEngine;
+
+    public class Entities
+    {
+        public static Lookup<string, StructureMaster> Structures
+        {
+            get
+            {
+                return (Lookup<string, StructureMaster>)StructureMaster.AllStructures
+                    .ToLookup(p => p.ownerID.ToString());
+            }
+        }
+
+        public static Lookup<string, BasicDoor> Doors
+        {
+            get 
+            {              
+                return (Lookup<string, BasicDoor>)(UnityEngine.Object.FindObjectsOfType(typeof(BasicDoor)) as BasicDoor[])
+                    .ToLookup(p => (p.GetComponent("DeployableObject") as DeployableObject).ownerID.ToString());
+            }
+        }
+    }
+}
+

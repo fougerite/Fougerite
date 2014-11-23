cb096101da63a0a518a74f45fbc76a469d1a57ca
Michael Crews <mike.crews@gmail.com>
2014-11-23 13:47:10 -0800

    support admin command help

--- RustPP/Commands/HelpCommand.cs
+++ RustPP/Commands/HelpCommand.cs
@@ -2,6 +2,7 @@
 {
     using Fougerite;
     using RustPP;
+    using RustPP.Permissions;
     using System;
 
     public class HelpCommand : ChatCommand
@@ -9,7 +10,7 @@
         public override void Execute(ref ConsoleSystem.Arg Arguments, ref string[] ChatArguments)
         {
             int num = 1;
-            do
+            do 
             {
                 string setting = Core.config.GetSetting("Settings", "help_string" + num);
                 if (setting != null)
@@ -17,12 +18,27 @@
                     Util.sayUser(Arguments.argUser.networkPlayer, Core.Name, setting);
                     num++;
                 }
+                else if (Administrator.IsAdmin(Arguments.argUser.userID))
+                {
+                    do
+                    {
+                        setting = Core.config.GetSetting("Settings", "admin_help_string" + num);
+                        if (setting != null)
+                        {
+                            Util.sayUser(Arguments.argUser.networkPlayer, Core.Name, setting);
+                            num++;
+                        }
+                        else
+                        {
+                            return;
+                        }
+                    } while(true);
+                }
                 else
                 {
-                    num = 0;
-                }
-            }
-            while (num != 0);
+                    return;
+                }               
+            } while (true);
         }
     }
 }

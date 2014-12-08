ea6fc6a2d02137b88d50dc3594d66e32f55ec0ef
Michael Crews <mike.crews@gmail.com>
2014-12-07 17:32:08 -0800

    loadout command gratuituous exception throwing fixed

--- RustPP/Commands/LoadoutCommand.cs
+++ RustPP/Commands/LoadoutCommand.cs
@@ -8,19 +8,27 @@
     {
         public override void Execute(ref ConsoleSystem.Arg Arguments, ref string[] ChatArguments)
         {
-            try
+            int items; 
+            if (int.TryParse(Core.config.GetSetting("AdminLoadout", "items"), out items))
             {
-                for (int i = 1; i < int.Parse(Core.config.GetSetting("AdminLoadout", "items") + 1); i++)
+                try
                 {
-                    Arguments.Args = new string[] { Core.config.GetSetting("AdminLoadout", "item" + i + "_name"), Core.config.GetSetting("AdminLoadout", "item" + i + "_amount") };
-                    inv.give(ref Arguments);
+                    for (int i = 1; i <= items; i++)
+                    {
+                        string name = Core.config.GetSetting("AdminLoadout", "item" + i + "_name");
+                        string amount = Core.config.GetSetting("AdminLoadout", "item" + i + "_amount");
+                        if (name.Length > 1 && amount.Length > 1)
+                        {
+                            Arguments.Args = new string[] { name, amount };
+                        }
+                        inv.give(ref Arguments);
+                    }
+                } catch (Exception ex)
+                {
+                    Logger.LogException(ex);
                 }
+                Util.sayUser(Arguments.argUser.networkPlayer, Core.Name, "You have spawned an Admin Loadout!");
             }
-            catch (Exception ex)
-            {
-                Logger.LogException(ex);
-            }
-            Util.sayUser(Arguments.argUser.networkPlayer, Core.Name, "You have spawned an Admin Loadout!");
         }
     }
 }

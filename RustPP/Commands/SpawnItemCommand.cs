8440cec34f8182fe8d89f40f2583b10f63fd8578
Michael Crews <mike.crews@gmail.com>
2014-12-18 12:59:58 -0800

    use the new MatchItemName method

--- RustPP/Commands/SpawnItemCommand.cs
+++ RustPP/Commands/SpawnItemCommand.cs
@@ -8,18 +8,22 @@
     {
         public override void Execute(ref ConsoleSystem.Arg Arguments, ref string[] ChatArguments)
         {
-            string itemName = World.GetWorld().ParseItemName(string.Join(" ", ChatArguments));
-            int qty;
-            if (!int.TryParse(ChatArguments[ChatArguments.Length - 1], out qty))
-                qty = 1;
-
-            if (!(itemName == string.Empty))
+            if (ChatArguments.Length > 0)
             {
+                string terms = ChatArguments[0];
+                for (int i = 1; i < ChatArguments.Length - 1; i++)
+                    terms += string.Format(" {0}", ChatArguments[i]);
+
+                string itemName = World.GetWorld().MatchItemName(terms.Trim());
+                int qty;
+                if (!int.TryParse(ChatArguments[ChatArguments.Length - 1], out qty))
+                    qty = 1;
+
                 Arguments.Args = new string[] { itemName, qty.ToString() };
                 inv.give(ref Arguments);
                 Util.sayUser(Arguments.argUser.networkPlayer, Core.Name, string.Format("{0}  {1} were placed in your inventory.", qty.ToString(), itemName));
-            }
-            else
+
+            } else
             {
                 Util.sayUser(Arguments.argUser.networkPlayer, Core.Name, "Spawn Item usage:  /i itemName quantity");
             }

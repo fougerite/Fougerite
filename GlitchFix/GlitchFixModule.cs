a17ff0ee626539af16f4c2b0c81eac27945971e3
dretax <dretax@users.noreply.github.com>
2015-01-29 17:39:52 +0100

    Fixes

--- GlitchFix/GlitchFixModule.cs
+++ GlitchFix/GlitchFixModule.cs
@@ -1,11 +1,6 @@
-using System.Linq;
-using Rust;
 using Fougerite;
-using Fougerite.Events;
 using System;
 using System.IO;
-using System.Collections.Generic;
-using System.Text;
 using System.Reflection;
 
 namespace GlitchFix
@@ -16,79 +11,104 @@ namespace GlitchFix
         {
             get { return "GlitchFix"; }
         }
+
         public override string Author
         {
             get { return "DreTaX"; }
         }
+
         public override string Description
         {
             get { return "Fixing multiply ramp spawning one over one"; }
         }
+
         public override Version Version
         {
             get { return Assembly.GetExecutingAssembly().GetName().Version; }
         }
+
         public override uint Order
         {
             get { return 2; }
         }
 
-        IniParser Config;
+        private IniParser Config;
+
         public override void Initialize()
         {
             Config = new IniParser(Path.Combine(ModuleFolder, "GlitchFix.cfg"));
-            Fougerite.Hooks.OnEntityDeployed += EntityDeployed;
+            if (Config.GetSetting("Settings", "enabled").ToLower() == "true")
+                Fougerite.Hooks.OnEntityDeployed += EntityDeployed;
         }
 
-        void EntityDeployed(Fougerite.Player Player, Fougerite.Entity Entity)
+        public override void DeInitialize()
         {
-            if (Config.GetSetting("Settings", "enabled").ToLower() == "true" && Entity != null)
+            if (Config.GetSetting("Settings", "enabled").ToLower() == "true")
+                Fougerite.Hooks.OnEntityDeployed -= EntityDeployed;
+        }
+
+        public void EntityDeployed(Fougerite.Player Player, Fougerite.Entity Entity)
+        {
+            if (Entity != null)
+            {
                 if (Entity.Name.Contains("Foundation") || Entity.Name.Contains("Ramp"))
                 {
                     var name = Entity.Name;
                     bool GiveBack = Config.GetSetting("Settings", "giveback").ToLower() == "true";
                     var two = Util.GetUtil().CreateVector(Entity.X, Entity.Y, Entity.Z);
-                    var deployable = Util.GetUtil().TryFindReturnType("DeployableObject");
-                    var deploylist = UnityEngine.Resources.FindObjectsOfTypeAll(deployable);
-                    if ((from ent in deploylist.Cast<DeployableObject>() where ent.name == "Wood Box" || ent.name == "Wood Box Large" let dist = Util.GetUtil().GetVectorsDistance(two, ent.gameObject.transform.position) where Entity.InstanceID != ent.GetInstanceID() && dist <= 2.5 select ent).Any())
+                    var deploylist = UnityEngine.Object.FindObjectsOfType(typeof (DeployableObject));
+                    foreach (DeployableObject ent in deploylist)
                     {
-                        if (Player != null && GiveBack)
+                        if (ent.name.Contains("WoodBox") && Entity.InstanceID != ent.GetInstanceID())
                         {
-                            switch (name)
+                            var dist = Util.GetUtil().GetVectorsDistance(two, ent.gameObject.transform.position);
+                            if (dist > 2.5) continue;
+                            if (Player != null && GiveBack)
                             {
-                                case "WoodFoundation":
-                                    name = "Wood Foundation";
-                                    break;
-                                case "MetalFoundation":
-                                    name = "Metal Foundation";
-                                    break;
+                                switch (name)
+                                {
+                                    case "WoodFoundation":
+                                        name = "Wood Foundation";
+                                        break;
+                                    case "MetalFoundation":
+                                        name = "Metal Foundation";
+                                        break;
+                                }
+                                Player.Inventory.AddItem(name, 1);
                             }
-                            Player.Inventory.AddItem(name, 1);
+                            Entity.Destroy();
+                            return;
                         }
-                        Entity.Destroy();
-                        return;
                     }
-                    var structure = Util.GetUtil().TryFindReturnType("StructureComponent");
-                    var structurelist = UnityEngine.Resources.FindObjectsOfTypeAll(structure);
-                    if ((from ent in structurelist.Cast<StructureComponent>() where ent.name == "WoodRamp" || ent.name == "MetalRamp" let dist = Util.GetUtil().GetVectorsDistance(ent.gameObject.transform.position, two) where Entity.InstanceID != ent.GetInstanceID() && dist == 0 select ent).Any())
+                    var structurelist = UnityEngine.Object.FindObjectsOfType(typeof (StructureComponent));
+                    foreach (StructureComponent structure in structurelist)
                     {
-                        if (GiveBack && Player != null)
+                        if (structure.name.Contains("Ramp") && Entity.InstanceID != structure.GetInstanceID())
                         {
-                            switch (name)
                             {
-                                case "WoodFoundation":
-                                    name = "Wood Foundation";
-                                    break;
-                                case "MetalFoundation":
-                                    name = "Metal Foundation";
-                                    break;
+                                var dist = Util.GetUtil()
+                                    .GetVectorsDistance(two, structure.gameObject.transform.position);
+                                if (dist != 0) continue;
+                                if (GiveBack && Player != null)
+                                {
+                                    switch (name)
+                                    {
+                                        case "WoodFoundation":
+                                            name = "Wood Foundation";
+                                            break;
+                                        case "MetalFoundation":
+                                            name = "Metal Foundation";
+                                            break;
+                                    }
+                                    Player.Inventory.AddItem(name, 1);
+                                }
+                                Entity.Destroy();
+                                return;
                             }
-                            Player.Inventory.AddItem(name, 1);
                         }
-                        Entity.Destroy();
-                        return;
                     }
                 }
+            }
         }
     }
 }
